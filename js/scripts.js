49ca7b24c7d644767fcd5f1895e6497920b1c662
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-02-08 19:20:47 -0800

    add capacity for user to delete text from page using js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
   $("button#cat").click(function() {
     $("ul#user").append("<li>Meow!</li>");
-    $("ul#webpage").append("<li>Mrowr!</li>");
+    $("ul#cat-response").append("<li>Mrowr!</li>");
+    $("ul#user").children("li").click(function() {
+    $(this).remove();
+    });
+    $("ul#cat-response").children("li").click(function() {
+    $(this).remove();
+    });
   });
 
   $("button#dog").click(function() {
     $("ul#user2").append("<li>Woof!</li>");
-    $("ul#webpage2").append("<li>Ruff! Ruff!</li>");
+    $("ul#dog-response").append("<li>Ruff! Ruff!</li>");
+    $("ul#user2").children("li").click(function() {
+    $(this).remove();
+    });
+    $("ul#dog-response").children("li").click(function() {
+    $(this).remove();
+    });
   });
 });
